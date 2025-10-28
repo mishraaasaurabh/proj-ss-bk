e02c3be4b9aded182d61e06f6048146075cb53e2
Saurabh <saurabhmishraald03@gmail.com>
2025-10-29 03:55:17 +0530

    consistent functionality

--- src/index.js
+++ src/index.js
@@ -10,8 +10,16 @@ app.use(express.urlencoded({extended: true}))
 
 app.use('/api',apiRoutes);
 
-app.listen(ServerConfig.PORT,()=>{
+app.listen(ServerConfig.PORT, async ()=>{
     console.log("Server runnig at port",ServerConfig.PORT)
     Logger.info("Success", {})
+    const { City, Airport} = require('./models')
+    const bengaluru =  await City.findByPk(5)
+    console.log(bengaluru)
+    const mumairport = await bengaluru.createAirport({name: 'Hubbli Airport',code: 'HBL'})
+    // const airport  = await Airport.create({name: "Prayagraj Airport", code: "PRYJ", cityId: 1})
+
+    // const airportsinbeng = await mumbai.getAirports();
+    // console.log(airportsinbeng)f
 })
 
